cf5e6bd3665ec849798716d0e36cbe59c50b3c18
NDICHU <ndichumuriithi@gmail.com>
2022-06-11 15:57:05 +0300

    Completed assignment

--- .history/lib/regex_lab_20220611155518.rb
+++ .history/lib/regex_lab_20220611155518.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-    word.match(/^[aeiouAEIOU]\w+/) ? true : false
+    word.match(/^[aeiou]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false   
+
 end
